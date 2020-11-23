1ef756dd8d35517ba5aecc518920f0f04d3a561d
Marc Kohaupt <debuglevel@gmail.com>
2020-11-23 23:54:42 +0100

    Simplify getting start and end vertices

--- src/main/kotlin/de/debuglevel/graphlibrary/GraphUtils.kt
+++ src/main/kotlin/de/debuglevel/graphlibrary/GraphUtils.kt
@@ -43,47 +43,17 @@ object GraphUtils {
     fun <T : Any> getStartVertices(graph: Graph<T>): List<Vertex<T>> {
         logger.trace { "Getting start vertices for graph..." }
         val startVertices = graph.getVertices()
-            .flatMap { vertex -> getStartVertices(vertex) }
-            .distinct()
+            .filter { it.inEdges.count() == 0 }
         logger.trace { "Got ${startVertices.count()} start vertices for graph" }
         return startVertices
-    }
-
-    fun <T : Any> getStartVertices(vertex: Vertex<T>): List<Vertex<T>> {
-        logger.trace { "Getting start vertices for vertex '$vertex'..." }
-        return if (vertex.inEdges.isEmpty()) {
-            logger.trace { "Vertex '$vertex' is the start of the graph" }
-            listOf(vertex)
-        } else {
-            val predecessors = vertex.inEdges
-                .flatMap { edge -> getStartVertices(edge.start) }
-                .distinct()
-            logger.trace { "Vertex '$vertex' is preceded by following vertices: ${predecessors.joinToString(", ")}" }
-            predecessors
-        }
     }
 
     fun <T : Any> getEndVertices(graph: Graph<T>): List<Vertex<T>> {
         logger.trace { "Getting end vertices for graph..." }
         val endVertices = graph.getVertices()
-            .flatMap { vertex -> getEndVertices(vertex) }
-            .distinct()
+            .filter { it.outEdges.count() == 0 }
         logger.trace { "Got ${endVertices.count()} end vertices for graph" }
         return endVertices
-    }
-
-    fun <T : Any> getEndVertices(vertex: Vertex<T>): List<Vertex<T>> {
-        logger.trace { "Getting end vertices for vertex '$vertex'..." }
-        return if (vertex.outEdges.isEmpty()) {
-            logger.trace { "Vertex '$vertex' is the end of the graph" }
-            listOf(vertex)
-        } else {
-            val successors = vertex.outEdges
-                .flatMap { edge -> getEndVertices(edge.end) }
-                .distinct()
-            logger.trace { "Vertex '$vertex' is preceded by following vertices: ${successors.joinToString(", ")}" }
-            successors
-        }
     }
 
     fun <T : Any> populateOrders(graph: Graph<T>) {
