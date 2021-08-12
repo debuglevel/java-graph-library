191b3393aaf67a4abf66119b1eab13d432161d0e
Marc Kohaupt <debuglevel@gmail.com>
2021-08-12 18:40:52 +0200

    Write into an OutputStream instead of a File

--- src/main/kotlin/de/debuglevel/graphlibrary/export/GraphvizExporter.kt
+++ src/main/kotlin/de/debuglevel/graphlibrary/export/GraphvizExporter.kt
@@ -3,7 +3,7 @@ package de.debuglevel.graphlibrary.export
 import guru.nidi.graphviz.engine.Format
 import guru.nidi.graphviz.engine.Graphviz
 import mu.KotlinLogging
-import java.io.File
+import java.io.OutputStream
 
 /**
  * Renders the graph via a Java graphviz port
@@ -14,17 +14,17 @@ object GraphvizExporter {
     /**
      * Renders a dot graph definition to a media file.
      * @param dot the graphviz dot source
-     * @param outputFile the destination media file
+     * @param outputStream OutputStream to write into
      * @param format media format to render to
      */
-    fun render(dot: String, outputFile: File, format: Format) {
-        logger.debug { "Rendering graph visualization to file '$outputFile'..." }
+    fun render(dot: String, outputStream: OutputStream, format: Format) {
+        logger.debug { "Rendering graph visualization to OutputStream '$outputStream'..." }
 
         val graph = guru.nidi.graphviz.parse.Parser.read(dot)
         Graphviz
             .fromGraph(graph)
             .render(format)
-            .toFile(outputFile)
+            .toOutputStream(outputStream)
 
         logger.debug { "Rendering graph visualization done." }
     }
